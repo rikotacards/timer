a6658736cd3429fd91e1c0fc2026e7e57a3f2b54
Max Hsu <Max.hsu@digitalasset.com>
2024-04-12 12:41:29 +0900

    add circular.

--- src/components/ActiveEntry.tsx
+++ src/components/ActiveEntry.tsx
@@ -1,4 +1,4 @@
-import { Alert, Box, Button, Card, Chip, Typography } from '@mui/material';
+import { Alert, Box, Button, Card, Chip, CircularProgress, Typography } from '@mui/material';
 import React from 'react';
 import { useAppDataContext, useSnackbarContext } from '../Providers/contextHooks';
 import { formatTime } from '../utils/formatTime';
@@ -9,11 +9,11 @@ import { useStopwatch } from 'react-timer-hook';
 export const ActiveEntry: React.FC = () => {
     const s = useSnackbarContext();
     const { setOpenEntry, openEntry, isLoadingActiveEntry } = useAppDataContext()
-    const now = new Date().getTime() 
+    const now = new Date().getTime()
     const [hide, setHide] = React.useState(false);
     const curr = new Date()
-    const dif = now- openEntry.startTime.seconds * 1000
-    curr.setSeconds(curr.getSeconds() + dif/1000);
+    const dif = now - openEntry.startTime.seconds * 1000
+    curr.setSeconds(curr.getSeconds() + dif / 1000);
     const { totalSeconds, pause, reset } = useStopwatch({ autoStart: true, offsetTimestamp: curr });
     const formatted = formatTime(totalSeconds)
     if (isLoadingActiveEntry) {
@@ -22,7 +22,7 @@ export const ActiveEntry: React.FC = () => {
     if (!openEntry) {
         return null
     }
-    if(hide){
+    if (hide) {
         return null
     }
     const onStop = async () => {
@@ -48,25 +48,29 @@ export const ActiveEntry: React.FC = () => {
         }
 
     }
-    
-    // {autoStart: true, offsetTimeStamp: new Date().setSeconds(openEntry.startTime.seconds+dif)}
-return (
-        <Card variant='outlined'  sx={{zIndex: '2000', p: 1, background: 'transparent', backdropFilter: 'blur(20px)' }}>
-            <Box sx={{zIndex:'1000', display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-                <Box sx={{display: 'flex',flexDirection: 'row', width: '100%'}}>
 
-                <Box sx={{ display: 'flex', width: '100%', flexDirection: 'column', alignItems: 'flex-start' }}>
-                    <Typography>{openEntry.desc}</Typography>
-                    <Box>
-                        {openEntry?.categories?.map((c,i) => <Chip size='small' key={c.categoryId + i} sx={{mb:1, background: c.color }} label={c.categoryName} />)}
+    // {autoStart: true, offsetTimeStamp: new Date().setSeconds(openEntry.startTime.seconds+dif)}
+    return (
+        <Card variant='outlined' sx={{ zIndex: '2000', p: 1, background: 'transparent', backdropFilter: 'blur(20px)' }}>
+            <Box sx={{ zIndex: '1000', display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+                <Box sx={{ display: 'flex', flexDirection: 'row', width: '100%' }}>
+
+                    <Box sx={{ display: 'flex', width: '100%', flexDirection: 'column', alignItems: 'flex-start' }}>
+                        <Typography variant='body1'>{openEntry.desc}</Typography>
+                        <Box>
+                            {openEntry?.categories?.map((c, i) => <Chip size='small' key={c.categoryId + i} sx={{ mb: 1, background: c.color }} label={c.categoryName} />)}
+                        </Box>
                     </Box>
-                </Box>
-                    <Box sx={{display: 'flex', ml: 'auto'}}>
+                    <Box sx={{ display: 'flex', ml: 'auto', alignItems: 'flex-start' }}>
+                        <Box sx={{alignItems: 'center', display: 'flex', }}>
+
+                        <CircularProgress color='success' sx={{mr:0.5}} size='1rem'/>
                         <Typography variant='h6' fontWeight={'bold'}>{formatted}</Typography>
+                        </Box>
                     </Box>
                 </Box>
-                    <Button size='large' onClick={onStop} color='warning' fullWidth variant='contained'>Stop</Button>
-                
+                <Button size='large' onClick={onStop} color='warning' fullWidth variant='contained'>Stop</Button>
+
 
             </Box>
 
